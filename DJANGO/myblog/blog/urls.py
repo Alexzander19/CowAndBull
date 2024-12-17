191a7e63835ae344a6bcfe6ff8209a525eb25051
Alexzander19 <2961781@mail.ru>
2024-12-17 20:51:54 +0700

    Какие-то изменения

--- DJANGO/myblog/blog/urls.py
+++ DJANGO/myblog/blog/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+
+     # ex: /blog/5/
+    path("<int:question_id>/", views.title, name="title"),
+    # ex: /blog/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+   
 ]
